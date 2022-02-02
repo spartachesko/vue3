10f5dcec06480655434fa142846f500cb5dfe731
borisov_spartak <fichker@gmail.com>
2022-02-02 19:30:06 +0300

    ProductItem  в процессе

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -1,14 +1,22 @@
 <template>
-    
+  <a class="catalog__pic" href="#">
+    <img :src="product.image"  :alt="product.title">
+  </a>
 
-    
+  <h3 class="catalog__title">
+    <a href="#">
+      {{ product.title }}
+    </a>
+  </h3>
+
+  <span class="catalog__price">
+    {{ product.price }}
+  </span>
 </template>
 
-
-
-
 <script>
-export default{
 
+export default{
+    props: ['product', 'index']
 }
 </script>
