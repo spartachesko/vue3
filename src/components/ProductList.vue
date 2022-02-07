2355f5da2ad702a22c5fe900cbdcd86b42573b74
borisov_spartak <fichker@gmail.com>
2022-02-07 23:59:23 +0300

    возник вопрос по фильтрации

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -2,33 +2,9 @@
 <section class="catalog">
 <ul class="catalog__list">
     <ProductItem
-    v-for="(product, index) in products"
-    :key="index"
-    :product = "product"
-    :index = "index"/>
-  <ul class="colors colors--black">
-    <li class="colors__item">
-      <label class="colors__label">
-      <input class="colors__radio sr-only" type="radio" name="color-1" value="#73B6EA" checked="">
-      <span class="colors__value" style="background-color: #73B6EA;">
-      </span>
-    </label>
-    </li>
-    <li class="colors__item">
-      <label class="colors__label">
-        <input class="colors__radio sr-only" type="radio" name="color-1" value="#8BE000">
-        <span class="colors__value" style="background-color: #8BE000;">
-        </span>
-      </label>
-    </li>
-    <li class="colors__item">
-      <label class="colors__label">
-        <input class="colors__radio sr-only" type="radio" name="color-1" value="#222">
-        <span class="colors__value" style="background-color: #222;">
-        </span>
-      </label>
-    </li>
-  </ul>
+    v-for="product in products"
+    :key="product.id"
+    :product = "product"/>
   </ul>
 </section>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,8 @@
 import Vue from 'vue';
 import App from './App.vue';
 
-import data from './data';
-import ShowMessage from './data2';
-
 Vue.config.productionTip = false;
 
 new Vue({
   render: (h) => h(App),
 }).$mount('#app');
-
-ShowMessage(data.message);
