23472b7f06595b17300f32decac30eab45e509bb
borisov_spartak <fichker@gmail.com>
2022-02-21 20:15:35 +0300

    Работа с фильтрацией цвета

--- src/components/ProductFilter.vue
+++ src/components/ProductFilter.vue
@@ -42,11 +42,48 @@
                   <input class="colors__radio sr-only"
                   type="radio"
                   name="color"
-                  value="valueColor"
+                  value="#73B6EA"
                   checked="">
                   <span class="colors__value" style="background-color: #73B6EA;">
                   </span>
                 </label>
+              </li>
+              <li class="colors__item">
+                <label class="colors__label">
+                  <input class="colors__radio sr-only" type="radio" name="color" value="#FFBE15">
+                  <span class="colors__value" style="background-color: #FFBE15;">
+                  </span>
+                </label>
+              </li>
+              <li class="colors__item">
+                <label class="colors__label">
+                  <input class="colors__radio sr-only" type="radio" name="color" value="#939393">
+                  <span class="colors__value" style="background-color: #939393;">
+                </span></label>
+              </li>
+              <li class="colors__item">
+                <label class="colors__label">
+                  <input class="colors__radio sr-only" type="radio" name="color" value="#8BE000">
+                  <span class="colors__value" style="background-color: #8BE000;">
+                </span></label>
+              </li>
+              <li class="colors__item">
+                <label class="colors__label">
+                  <input class="colors__radio sr-only" type="radio" name="color" value="#FF6B00">
+                  <span class="colors__value" style="background-color: #FF6B00;">
+                </span></label>
+              </li>
+              <li class="colors__item">
+                <label class="colors__label">
+                  <input class="colors__radio sr-only" type="radio" name="color" value="#FFF">
+                  <span class="colors__value" style="background-color: #FFF;">
+                </span></label>
+              </li>
+              <li class="colors__item">
+                <label class="colors__label">
+                  <input class="colors__radio sr-only" type="radio" name="color" value="#000">
+                  <span class="colors__value" style="background-color: #000;">
+                </span></label>
               </li>
             </ul>
           </fieldset>
