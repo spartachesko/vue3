4baf3b808b2c8b35a09edc6eddefbc13f95048aa
borisov_spartak <fichker@gmail.com>
2022-03-07 23:20:51 +0300

    возникла ошибка для routes

--- src/App.vue
+++ src/App.vue
@@ -1,77 +1,34 @@
 <template>
-    <main class="content container">
-    <div class="content__top content__top--catalog">
-      <h1 class="content__title">
-        Каталог
-      </h1>
-      <span class="content__info">
-        152 товара
-      </span>
-    </div>
-
-    <div class="content__catalog">
-      <ProductFilter
-      :price-from.sync="filterPriceFrom"
-      :price-to.sync="filterPriceTo"
-      :category-id.sync = "filterCategoryId"
-      :color.sync = "filterColor"/>
-        <section class="catalog">
-  <ProductList :products = "products"/>
-  <BasePagination v-model ="page"
-  :count="countProducts"
-  :per-page="productsPerPage" />
-  </section>
-    </div>
-  </main>
+  <component :is="currentPageComponent"/>
 </template>
 
 <script>
-import products from './data/products';
-import ProductList from './components/ProductList.vue';
-import BasePagination from './components/BasePagination.vue';
-import ProductFilter from './components/ProductFilter.vue';
+import MainPage from '@/pages/MainPage.vue';
+import ProductPage from '@/pages/ProductPage.vue';
+import NotFoundPage from '@/pages/NotFoundPage.vue';
+
+const routes = {
+  main: 'MainPage',
+  product: 'ProductPage',
+};
 
 export default {
-  name: 'App',
-  components: { ProductList, BasePagination, ProductFilter },
   data() {
     return {
-      filterPriceFrom: 0,
-      filterPriceTo: 0,
-      filterCategoryId: 0,
-      filterColor: 0,
-      page: 1,
-      productsPerPage: 3,
+      currentPage: 'main',
     };
   },
+
   computed: {
-    filteredProducts() {
-      let filterProducts = products;
-      if (this.filterPriceFrom > 0) {
-        filterProducts = filterProducts.filter((product) => product.price > this.filterPriceFrom);
-      }
-      if (this.filterPriceTo > 0) {
-        filterProducts = filterProducts.filter((product) => product.price < this.filterPriceTo);
-      }
-      if (this.filterCategoryId) {
-        filterProducts = filterProducts.filter(
-          (product) => product.categoryId === this.filterCategoryId,
-        );
-      }
-      if (this.filterColor) {
-        filterProducts = filterProducts.filter(
-          (product) => product.colors.some((color) => color === this.filterColor),
-        );
-      }
-      return filterProducts;
+    currentPageComponent() {
+      return routes(this.currentPage) || 'NotFoundPage';
     },
-    products() {
-      const offset = (this.page - 1) * this.productsPerPage;
-      return this.filteredProducts.slice(offset, offset + this.productsPerPage);
-    },
-    countProducts() {
-      return this.filteredProducts.length;
-    },
+  },
+
+  components: {
+    MainPage,
+    ProductPage,
+    NotFoundPage,
   },
 };
 </script>
