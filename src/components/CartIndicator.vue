2324e77332bcf474c698c20c7eb8b7d017404a57
borisov_spartak <fichker@gmail.com>
2022-03-16 15:00:51 +0300

    Добавил CartPage

---
+++ src/components/CartIndicator.vue
@@ -0,0 +1,14 @@
+<template>
+        <router-link class="header__cart" aria-label="Корзина с товарами" :to="{name: 'cart'}">
+        <svg width="30" height="21" fill="currentColor">
+          <use xlink:href="#icon-cart"></use>
+        </svg>
+        <span class="header__count" aria-label="Количество товаров">3</span>
+      </router-link>
+</template>
+
+<script>
+export default {
+
+};
+</script>
