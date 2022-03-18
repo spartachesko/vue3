4863c6bad891adfa62998d6bb2a74c33747f48c1
borisov_spartak <fichker@gmail.com>
2022-03-18 16:22:02 +0300

    пагинация API

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,26 +1,25 @@
 <template>
-<main class="content container">
+  <main class="content container">
     <div class="content__top content__top--catalog">
-      <h1 class="content__title">
-        Каталог
-      </h1>
-      <span class="content__info">
-        152 товара
-      </span>
+      <h1 class="content__title">Каталог</h1>
+      <span class="content__info"> 152 товара </span>
     </div>
 
     <div class="content__catalog">
       <ProductFilter
-      :price-from.sync="filterPriceFrom"
-      :price-to.sync="filterPriceTo"
-      :category-id.sync = "filterCategoryId"
-      :color.sync = "filterColor"/>
-        <section class="catalog">
-  <ProductList :products = "products"/>
-  <BasePagination v-model ="page"
-  :count="countProducts"
-  :per-page="productsPerPage" />
-  </section>
+        :price-from.sync="filterPriceFrom"
+        :price-to.sync="filterPriceTo"
+        :category-id.sync="filterCategoryId"
+        :color.sync="filterColor"
+      />
+      <section class="catalog">
+        <ProductList :products="products" />
+        <BasePagination
+          v-model="page"
+          :count="countProducts"
+          :per-page="productsPerPage"
+        />
+      </section>
     </div>
   </main>
 </template>
@@ -50,10 +49,14 @@ export default {
     filteredProducts() {
       let filterProducts = products;
       if (this.filterPriceFrom > 0) {
-        filterProducts = filterProducts.filter((product) => product.price > this.filterPriceFrom);
+        filterProducts = filterProducts.filter(
+          (product) => product.price > this.filterPriceFrom,
+        );
       }
       if (this.filterPriceTo > 0) {
-        filterProducts = filterProducts.filter((product) => product.price < this.filterPriceTo);
+        filterProducts = filterProducts.filter(
+          (product) => product.price < this.filterPriceTo,
+        );
       }
       if (this.filterCategoryId) {
         filterProducts = filterProducts.filter(
@@ -61,30 +64,41 @@ export default {
         );
       }
       if (this.filterColor) {
-        filterProducts = filterProducts.filter(
-          (product) => product.colors.some((color) => color === this.filterColor),
-        );
+        filterProducts = filterProducts.filter((p) => p.colors.some((c) => c === this.filterColor));
       }
+
       return filterProducts;
     },
     products() {
-      const offset = (this.page - 1) * this.productsPerPage;
-      return this.filteredProducts.slice(offset, offset + this.productsPerPage);
+      return this.productsData
+        ? this.productsData.items.map((product) => ({
+          ...product,
+          image: product.image.file.url,
+        })) : [];
     },
     countProducts() {
-      return this.filteredProducts.length;
+      return this.productsData ? this.productsData.pagination.total : 0;
     },
   },
 
   methods: {
     loadProducts() {
-      axios.get('https://vue-study.skillbox.cc/api/products')
-        .then((response) => { this.productsData = response.data; });
+      axios
+        .get(`https://vue-study.skillbox.cc/api/products?page=${this.page}&limit=${this.productsPerPage}`)
+        .then((response) => {
+          this.productsData = response.data;
+        });
     },
   },
 
-  create() {
-    this.loadProducts();
+  watch: {
+    page() {
+      this.loadProducts();
+    },
+  },
+
+  async created() {
+    await this.loadProducts();
   },
 };
 </script>
